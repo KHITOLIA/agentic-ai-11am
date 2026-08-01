b80b71ba2c5f8ca1227b582962f62d8464ec72af
Tushar Khitoliya <chitkarauniversity390@gmail.com>
2026-08-01 11:29:39 +0530

    new changes

--- ChatModels/chatmodel_openai.py
+++
@@ -1,7 +0,0 @@
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-load_dotenv()
-
-model = ChatOpenAI(model = "gpt-4", temperature = 0, max_completion_tokens = 1000)
-result = model.invoke("What is machine learning")
-print(result.content)
