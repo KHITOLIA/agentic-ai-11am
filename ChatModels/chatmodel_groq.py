73a190babdff8a0d928f85a3af89dbc60b0651e1
Tushar Khitoliya <chitkarauniversity390@gmail.com>
2026-08-06 18:04:03 +0530

    groq model

--- ChatModels/chatmodel_groq.py
+++ ChatModels/chatmodel_groq.py
@@ -2,6 +2,6 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 load_dotenv()
 
-model = ChatGroq(model = "meta-llama/llama-4-scout-17b-16e-instruct", temperature = 0)
+model = ChatGroq(model = "llama-3.3-70b-versatile", temperature = 0)
 result = model.invoke("What is machine learning ? ")
-print(result)
+print(result.content)
